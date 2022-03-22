248967228d58bde7b118826b213d21c375f06374
Nicolas Toews <toewsnicolas@gmail.com>
2022-03-22 12:12:41 -0300

    Unbuffered channel deadlock error

---
+++ Unbuffered-channel-deadlock-error/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	ca := make(chan int)////El int indica el tipo de dato que el canal va a estar enviando o recibiendo
+	//ca es una variable
+	ca <- 42 //Con esta linea decimos que le mandamos al canal llamado ca, el valor 42
+
+	fmt.Println(ca)
+
+}
